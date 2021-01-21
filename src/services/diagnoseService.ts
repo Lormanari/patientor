7f1744747a77bec627bdf1b71915929c207e84a9
Yurong Zhao <yurong.zhao@lianatech.com>
2021-01-21 22:27:04 +0200

    Exercises 9.8.-9.11.

---
+++ src/services/diagnoseService.ts
@@ -0,0 +1,10 @@
+import diagnoses from '../../data/diagnoses';
+import { diagnoseEntry } from '../types';
+
+const getEntries = (): diagnoseEntry[] => {
+  return diagnoses;
+};
+
+export default {
+  getEntries,
+};
